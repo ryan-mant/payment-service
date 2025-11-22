744652ee6b03b420fe4113134ab5fab8da0053a1
ryan-mant <ryan.tuglife@hotmail.com>
2025-11-22 15:57:47 -0300

    created exception handler

---
+++ core-banking-service/src/main/java/com/ryan_dev/core_banking_service/adapters/in/controller/advice/ExceptionHandlerAdvice.java
@@ -0,0 +1,75 @@
+package com.ryan_dev.core_banking_service.adapters.in.controller.advice;
+
+import com.ryan_dev.core_banking_service.application.domain.exceptions.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+import java.time.Instant;
+import java.util.List;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    private final Logger logger = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                "Invalid request data."
+        );
+
+        problemDetail.setTitle("Validation Exception");
+//        problemDetail.setType(URI.create("https://github.com/ryan-mant/payment-service")); IMPLEMENTAR UM LINK COM DOC
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        List<InvalidParam> invalidParams = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> new InvalidParam(
+                        error.getField(),
+                        error.getDefaultMessage()
+                ))
+                .toList();
+
+        problemDetail.setProperty("invalid_params", invalidParams);
+
+        return problemDetail;
+    }
+
+    record InvalidParam(String name, String reason) {}
+
+    @ExceptionHandler(BusinessException.class)
+    public ProblemDetail handleBusinessException(BusinessException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+
+        problemDetail.setTitle("Business Exception");
+//        problemDetail.setType(URI.create("https://github.com/ryan-mant/payment-service")); IMPLEMENTAR UM LINK COM DOC
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        problemDetail.setProperty("internal_code", e.getCode());
+
+        return problemDetail;
+    }
+
+
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleUnexpectedException(Exception ex) {
+        logger.error("An unexpected error occurred: ", ex);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "An unexpected error occurred. Please contact the administrator."
+        );
+
+        problemDetail.setTitle("Unexpected Exception");
+//        problemDetail.setType(URI.create("https://github.com/ryan-mant/payment-service")); IMPLEMENTAR UM LINK COM DOC
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+}
