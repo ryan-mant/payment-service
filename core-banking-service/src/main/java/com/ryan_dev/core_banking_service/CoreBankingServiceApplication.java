a86750e51554df2da95f7f56a548bbed58e1c1a6
ryan-mant <ryan.tuglife@hotmail.com>
2025-12-08 11:26:15 -0300

    enable feign only for my client package

--- core-banking-service/src/main/java/com/ryan_dev/core_banking_service/CoreBankingServiceApplication.java
+++ core-banking-service/src/main/java/com/ryan_dev/core_banking_service/CoreBankingServiceApplication.java
@@ -2,8 +2,10 @@ package com.ryan_dev.core_banking_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.ryan_dev.core_banking_service.adapters.out.client")
 public class CoreBankingServiceApplication {
 
 	public static void main(String[] args) {
