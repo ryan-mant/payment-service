808d1614912a76334724278587d2b3e3cb3d5076
ryan-mant <ryan.tuglife@hotmail.com>
2025-12-08 11:46:20 -0300

    update with authorizer

--- core-banking-service/src/main/java/com/ryan_dev/core_banking_service/application/service/TransferUseCaseImpl.java
+++ core-banking-service/src/main/java/com/ryan_dev/core_banking_service/application/service/TransferUseCaseImpl.java
@@ -1,8 +1,10 @@
 package com.ryan_dev.core_banking_service.application.service;
 
 import com.ryan_dev.core_banking_service.application.domain.Wallet;
+import com.ryan_dev.core_banking_service.application.domain.exceptions.BusinessException;
 import com.ryan_dev.core_banking_service.application.ports.in.transfer.commands.TransferCommand; // ✅ Importa do próprio Core
 import com.ryan_dev.core_banking_service.application.ports.in.transfer.TransferUseCase;
+import com.ryan_dev.core_banking_service.application.ports.out.AuthorizerPort;
 import com.ryan_dev.core_banking_service.application.ports.out.WalletRepositoryPort;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,11 @@ import org.springframework.stereotype.Service;
 public class TransferUseCaseImpl implements TransferUseCase {
 
     private final WalletRepositoryPort walletRepositoryPort;
+    private final AuthorizerPort authorizerPort;
 
-    public TransferUseCaseImpl(WalletRepositoryPort walletRepositoryPort) {
+    public TransferUseCaseImpl(WalletRepositoryPort walletRepositoryPort, AuthorizerPort authorizerPort) {
         this.walletRepositoryPort = walletRepositoryPort;
+        this.authorizerPort = authorizerPort;
     }
 
     @Override
@@ -27,6 +31,13 @@ public class TransferUseCaseImpl implements TransferUseCase {
                 .orElseThrow(() -> new RuntimeException("Payee not found"));
 
         payer.debit(command.amount());
+
+        boolean authorized = authorizerPort.authorize(payer, command.amount());
+
+        if (!authorized) {
+            throw new BusinessException("TRANSFER_REJECTED", "Transfer rejected by the authorizer.");
+        }
+
         payee.credit(command.amount());
 
         walletRepositoryPort.save(payer);
