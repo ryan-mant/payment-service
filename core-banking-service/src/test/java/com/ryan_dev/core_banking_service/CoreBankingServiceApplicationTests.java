83f705d8be6158d9324782c48ce189e1e2044b65
ryan-mant <ryan.tuglife@hotmail.com>
2026-02-02 12:58:55 -0300

    Configure Testcontainers for PostgreSQL and RabbitMQ

--- core-banking-service/src/test/java/com/ryan_dev/core_banking_service/CoreBankingServiceApplicationTests.java
+++ core-banking-service/src/test/java/com/ryan_dev/core_banking_service/CoreBankingServiceApplicationTests.java
@@ -2,12 +2,26 @@ package com.ryan_dev.core_banking_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class CoreBankingServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @Container
+    @ServiceConnection
+    static RabbitMQContainer rabbitmq = new RabbitMQContainer("rabbitmq:3.12-management");
+
+    @Test
+    void contextLoads() {
+    }
 
 }
