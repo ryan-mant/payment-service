f17ca127d01397a4f9268c2cd6aba0d58b0a0ec0
ryan-mant <ryan.tuglife@hotmail.com>
2026-02-28 21:21:48 -0300

    Remove existsByTransactionId from SpringDataNotificationRepository

--- notification-service/src/main/java/com/ryan_dev/notification_service/adapters/out/persistence/repository/SpringDataNotificationRepository.java
+++ notification-service/src/main/java/com/ryan_dev/notification_service/adapters/out/persistence/repository/SpringDataNotificationRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface SpringDataNotificationRepository extends JpaRepository<NotificationEntity, UUID> {
-
-    boolean existsByTransactionId(UUID transactionId);
 }
